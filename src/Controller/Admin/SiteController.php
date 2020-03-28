e53835b9ac265470a0e438b9bb492683bbd014ba
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2020-03-28 16:05:26 +0300

    admin namespace

---
+++ src/Controller/Admin/SiteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
+class SiteController extends AdminAbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/site/index.html.twig');
+    }
+
+    /**
+     * @Route("/login")
+     *
+     * @return Response
+     */
+    public function login(): Response
+    {
+        return $this->render('admin/site/login.html.twig');
+    }
+}
