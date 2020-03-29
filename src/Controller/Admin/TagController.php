b22a572e7b0c31db23c2291fab1f5df175df2533
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2020-03-29 16:48:25 +0300

    add tag

--- src/Controller/Admin/TagController.php
+++ src/Controller/Admin/TagController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Tag;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +17,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class TagController extends AdminAbstractController
 {
     /**
-     * @Route("/", name="admin.tag.index", methods={"GET"})
+     * @Route("/", name="admin.tag.index", methods={"GET", "HEAD"})
      */
     public function index(): Response
     {
@@ -19,31 +25,54 @@ class TagController extends AdminAbstractController
     }
 
     /**
-     * @Route("/{tag}", name="admin.tag.show", methods={"GET"})
+     * @Route("/{id}", name="admin.tag.show", methods={"GET", "HEAD"}, requirements={"id"="[0-9]+"})
+     *
+     * @param $id
+     * @return Response
      */
     public function show($id): Response
     {
+        dump($id);
         dd('show');
     }
 
     /**
-     * @Route("/create", name="admin.tag.create", methods={"GET"})
+     * @Route("/create", name="admin.tag.create", methods={"GET", "HEAD"})
      */
     public function create(): Response
     {
-        dd('create');
+        $tag = new Tag();
+
+        return $this->render('admin/tag/create.html.twig', [
+            'form' => $this->getForm($tag)->createView(),
+        ]);
     }
 
     /**
-     * @Route("/create", name="admin.tag.store", methods={"POST"})
+     * @Route("/create", name="admin.tag.store", methods={"POST", "PUT"})
+     *
+     * @param Request $request
+     * @return Response
      */
-    public function store(): Response
+    public function store(Request $request): Response
     {
-        dd('store');
+        $form = $this->getForm()->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin.tag.index');
+        }
+
+        return $this->render('admin/tag/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
-     * @Route("/{tag}/edit", name="admin.tag.edit", methods={"GET"})
+     * @Route("/{tag}/edit", name="admin.tag.edit", methods={"GET", "HEAD"})
      */
     public function edit(): Response
     {
@@ -51,7 +80,7 @@ class TagController extends AdminAbstractController
     }
 
     /**
-     * @Route("/{tag}/edit", name="admin.tag.update", methods={"POST"})
+     * @Route("/{tag}/edit", name="admin.tag.update", methods={"POST", "PUT"})
      */
     public function update(): Response
     {
@@ -64,5 +93,22 @@ class TagController extends AdminAbstractController
     public function destroy(): Response
     {
         dd('destroy');
+    }
+
+    /**
+     * @param $tag
+     * @return FormInterface
+     */
+    protected function getForm($tag = null): FormInterface
+    {
+        if ($tag === null) {
+            $tag = new Tag();
+        }
+
+        return $this->createFormBuilder($tag)
+            ->add('name', TextType::class, ['label' => 'Название'])
+            ->add('sort', IntegerType::class, ['label' => 'Сортировка'])
+            ->add('save', SubmitType::class, ['label' => 'Сохранить'])
+            ->getForm();
     }
 }
