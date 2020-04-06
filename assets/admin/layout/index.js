ca87072543593c79c4e1bd95c02a67b12672c078
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2020-04-06 12:09:55 +0300

    icons

--- assets/admin/layout/index.js
+++ assets/admin/layout/index.js
@@ -1,14 +1,4 @@
-import {library, dom} from '@fortawesome/fontawesome-svg-core';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-import {far} from '@fortawesome/free-regular-svg-icons';
-import {fab} from '@fortawesome/free-brands-svg-icons';
-
 import './index.scss';
-
-library.add(fas, far, fab);
-
-dom.i2svg().then(r => {
-});
 
 let coll = document.getElementsByClassName('collapsible');
 for (let i = 0; i < coll.length; i++) {
