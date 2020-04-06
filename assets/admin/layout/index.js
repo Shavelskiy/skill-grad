82d8f4038b4d7419293aa67815a7ba5b2daa6acd
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2020-04-06 11:51:13 +0300

    npm -> yarn. delete bootstrap depen

--- assets/admin/layout/index.js
+++ assets/admin/layout/index.js
@@ -5,9 +5,6 @@ import {fab} from '@fortawesome/free-brands-svg-icons';
 
 import './index.scss';
 
-const $ = require('jquery');
-require('bootstrap-datepicker');
-
 library.add(fas, far, fab);
 
 dom.i2svg().then(r => {
@@ -33,52 +30,22 @@ sidebarToggler.addEventListener('click', function () {
   content.classList.toggle('main--active');
 });
 
-const userProfileButton = $('.user-profile');
-const userProfileMenu = $('.user-profile-card');
+// const userProfileButton = $('.user-profile');
+// const userProfileMenu = $('.user-profile-card');
 
 const userProfileButtonDom = document.querySelector('.user-profile');
 const userProfileMenuDom = document.querySelector('.user-profile-card');
 
 
-$.fn.datepicker.dates['ru'] = {
-  days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-  daysShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-  daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-  months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-  monthsShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
-  today: 'Сегодня',
-  clear: 'Очистить',
-  format: 'mm.dd.yyyy',
-  titleFormat: 'MM yyyy',
-  weekStart: 0
-};
-
-
-$(document).ready(function () {
-  userProfileButton.click(function () {
-    userProfileMenu.toggleClass('user-profile-card-hidden');
-  });
-
-  $('.datepicker').datepicker({
-    language: 'ru',
-    orientation: 'bottom',
-  });
-
-  $('.filter-form-reset-btn').click(function () {
-    const filterForm = $('.filter-form');
-    filterForm.find('input').each(function (index, element) {
-      $(element).removeAttr('value');
-    });
-
-    filterForm.find('select').each(function (index, element) {
-      $(element).prop('selectedIndex', 0);
-    });
-  });
-});
-
-$(document).click(function (event) {
-  if (!userProfileMenuDom.contains(event.target) && !userProfileButtonDom.contains(event.target)) {
-    userProfileMenu.addClass('user-profile-card-hidden');
-  }
-});
-
+// $(document).ready(function () {
+//   userProfileButton.click(function () {
+//     userProfileMenu.toggleClass('user-profile-card-hidden');
+//   });
+//
+// });
+//
+// $(document).click(function (event) {
+//   if (!userProfileMenuDom.contains(event.target) && !userProfileButtonDom.contains(event.target)) {
+//     userProfileMenu.addClass('user-profile-card-hidden');
+//   }
+// });
