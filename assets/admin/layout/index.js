cf07e32b6015b23c0e8cc0b9914ca83f88ae4528
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2020-04-08 22:57:45 +0300

    user menu in admin

--- assets/admin/layout/index.js
+++ assets/admin/layout/index.js
@@ -13,29 +13,25 @@ for (let i = 0; i < coll.length; i++) {
   });
 }
 
-let sidebarToggler = document.getElementsByClassName('sidebar-toggler')[0];
+let sidebarToggler = document.querySelector('.sidebar-toggler');
 sidebarToggler.addEventListener('click', function () {
   this.classList.toggle('sidebar-toggler--active');
   let content = document.getElementsByClassName('main')[0];
   content.classList.toggle('main--active');
 });
 
-// const userProfileButton = $('.user-profile');
-// const userProfileMenu = $('.user-profile-card');
+const userProfileButton = document.querySelector('.user-profile');
+const userProfileMenu = document.querySelector('.user-profile-card');
 
 const userProfileButtonDom = document.querySelector('.user-profile');
 const userProfileMenuDom = document.querySelector('.user-profile-card');
 
+userProfileButton.addEventListener('click', function () {
+  userProfileMenu.classList.toggle('user-profile-card-hidden');
+});
 
-// $(document).ready(function () {
-//   userProfileButton.click(function () {
-//     userProfileMenu.toggleClass('user-profile-card-hidden');
-//   });
-//
-// });
-//
-// $(document).click(function (event) {
-//   if (!userProfileMenuDom.contains(event.target) && !userProfileButtonDom.contains(event.target)) {
-//     userProfileMenu.addClass('user-profile-card-hidden');
-//   }
-// });
+document.addEventListener('click', function (event) {
+  if (!userProfileMenuDom.contains(event.target) && !userProfileButtonDom.contains(event.target)) {
+    userProfileMenu.classList.add('user-profile-card-hidden');
+  }
+})
