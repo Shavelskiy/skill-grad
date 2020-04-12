45c2704e794a1363f2adf93c8b2929fd565c5653
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2020-04-12 16:15:06 +0300

    auth form styles

--- assets/components/auth/index.js
+++ assets/components/auth/index.js
@@ -17,18 +17,21 @@ if ((authModalBtn !== null) && (authModal !== null)) {
   }
 
   const authForm = authModal.querySelector('form');
-  const loginCsrfInput = document.getElementById('login-csrf');
+  const loginCsrfInput = authForm.querySelector('input[name="_csrf_token"]');
+  const authButton = authForm.querySelector('button');
 
   authForm.onsubmit = function (e) {
     e.preventDefault();
-    let formData = new FormData(this);
+    authButton.disabled = true;
 
-    axios.post('/ajax/login', formData)
+    axios.post('/ajax/login', new FormData(this))
       .then(response => {
         authModal.classList.remove('modal-active');
+        authButton.disabled = false;
       })
       .catch(error => {
         loginCsrfInput.value = error.response.data.csrf;
+        authButton.disabled = false;
       });
   }
 }
